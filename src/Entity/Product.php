d1b6921a6148ab2924271a0dd2bcb49140c03801
azeupaul <azeupaul@gmail.com>
2023-02-22 11:40:15 +0100

    Update first test and add a second test

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,6 +4,10 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 
 class Product{
     const FOOD_PRODUCT = 'food';
+    private $name;
+    private $type;
+    private $price;
+
     public function __construct($name, $type, $price)
     {
         $this->name = $name;
@@ -15,5 +19,6 @@ class Product{
         if (self::FOOD_PRODUCT == $this->type) {
             return $this->price * 0.055;
         }
+        return $this->price * 0.196;
     }
 }
--- tests/Entity/ProductTest.php
+++ tests/Entity/ProductTest.php
@@ -6,9 +6,15 @@ use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
-    public function testDefault()
+    public function testComputeTVAFoodProduct()
     {
         $product = new Product('Pomme', 'food', 1);
         $this->assertSame(0.055, $product->computeTVA());
+    }
+
+    public function testComputeTVAOtherProduct()
+    {
+        $product = new Product('Mangue', 'fruits', 20);
+        $this->assertSame(3.92, $product->computeTVA());
     }
 }
